814fb976a2b54c20712e476c4d71af81cdc5afa1
samuelitoxd7278 <secp.tdio@gmail.com>
2026-08-10 16:52:23 -0500

    cambio en el endpoint

---
+++ src/users/entities/rol.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 150, unique: true })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 255, select: false })
+  password!: string;
+
+  @Column({ type: 'boolean', default: true })
+  isActive!: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+}
